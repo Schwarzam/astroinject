7479772f0965a019c8a7e47a71eff95e5c1ba4bd
schwarzam <gustavo.b.schwarz@gmail.com>
2023-08-29 16:39:48 -0300

    added some docs and fixed minor on checkpoint

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='astroinject',
-    version='0.1',
+    version='0.2',
+    author="Gustavo Schwarz",
+    author_email="gustavo.b.schwarz@gmail.com",
     packages=find_packages(),
+    install_requires = ['numpy', 'pandas', 'psycopg2-binary', 'sqlalchemy', 'pyyaml'],
     entry_points={
         'console_scripts': [
             'astroinject=astroinject.main:main',
