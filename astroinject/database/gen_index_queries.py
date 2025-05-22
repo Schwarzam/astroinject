6d06e8ac58da3fbe9f47802c9812b05f9c3e8cf1
schwarzam <gustavo.b.schwarz@gmail.com>
2025-05-22 17:25:09 -0300

    added support to q3c index

--- astroinject/database/gen_index_queries.py
+++ astroinject/database/gen_index_queries.py
@@ -12,3 +12,18 @@ def make_pg_sphere_index(table, ra_col, dec_col):
   
     return query
 
+def make_q3c_index(table, ra_col, dec_col):
+    ra_col = ra_col.lower()
+    dec_col = dec_col.lower()
+
+    if "." in table:
+        wtdtable = table.split(".")[1]
+    else:
+        wtdtable = table
+
+    index_name = f"{wtdtable}_{ra_col}_{dec_col}_q3c_idx"
+
+    query = f"""CREATE INDEX {index_name} 
+    ON {table} (q3c_ang2ipix({ra_col}, {dec_col}));"""
+
+    return query
